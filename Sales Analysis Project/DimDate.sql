0fdd02749e6545b1a94ddcc88b47671e9e8a8b5d
Meryem Michraf <83471022+MeryemMichraf@users.noreply.github.com>
2023-04-25 19:04:10 -0400

    Add files via upload

---
+++ Sales Analysis Project/DimDate.sql
@@ -0,0 +1,15 @@
+--cleansed DimDate table // to visualize sales over time
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] as Date, 
+  [EnglishDayNameOfWeek] as Day, 
+  [WeekNumberOfYear] as WeekNo, 
+  [EnglishMonthName] as Month, 
+  left([EnglishMonthName], 3) as MonthShort, 
+  [MonthNumberOfYear] as MonthNo, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] AS Year 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+where 
+  [CalendarYear] >= 2019
